7fca8c26c0e40f421bc69c6aff348ef4bf486031
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-03-07 09:30:09 +0000

    Added GPL text.

--- mathjax-latex.php
+++ mathjax-latex.php
@@ -9,8 +9,28 @@
    Copyright 2010. Phillip Lord (phillip.lord@newcastle.ac.uk)
    Simon Cockell (s.j.cockell@newcastle.ac.uk)
    Newcastle University. 
-   
   */
+
+/*
+ * The contents of this file are subject to the LGPL License, Version 3.0.
+ *
+ * Copyright (C) 2010-2013, Phillip Lord, Newcastle University
+ * Copyright (C) 2010-2011, Simon Cockell, Newcastle University
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+
 
 
 class MathJax{
