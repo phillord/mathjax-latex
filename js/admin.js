0687bd353022b151001283b0f7a2fcca3ec823b6
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-03-13 13:52:50 +0000

    Admin javascript no longer active.

--- js/admin.js
+++
@@ -1,5 +0,0 @@
-function change_state() {
-    mathjaxlatex.default_disabled.disabled = mathjaxlatex.use_cdn.checked;
-    mathjaxlatex.mathjax_location.disabled = mathjaxlatex.use_cdn.checked;
-    mathjaxlatex.mathjax_config.disabled = mathjaxlatex.use_cdn.checked;
-}
