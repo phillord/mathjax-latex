8732b2e1d8887525904eca0e6c8168d88385dfa7
Paul Schreiber <paul.schreiber@espn.com>
2017-03-27 13:37:24 -0400

    update version and readme to 1.3.5; update 'tested up to' to 4.7.3

--- mathjax-latex.php
+++ mathjax-latex.php
@@ -2,7 +2,7 @@
 /*
  Plugin Name: MathJax-LaTeX
  Description: Transform latex equations in JavaScript using mathjax
- Version: 1.3.4
+ Version: 1.3.5
  Author: Phillip Lord, Simon Cockell, Paul Schreiber
  Author URI: http://knowledgeblog.org
 
@@ -32,7 +32,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-define( 'MATHJAX_VERSION', '1.3.4' );
+define( 'MATHJAX_VERSION', '1.3.5' );
 
 require_once( dirname( __FILE__ ) . '/mathjax-latex-admin.php' );
 
