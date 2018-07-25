f3652836d4d79d2c6f49036764cec7e41d0a8946
Paul Schreiber <paul.schreiber@espn.com>
2018-07-25 11:29:23 -0400

    add mathjax-latex.php stub

---
+++ mathjax-latex.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: MathJax-LaTeX
+ * Description: Transform latex equations in JavaScript using mathjax
+ * Version: 1.3.9
+ * Author: Phillip Lord, Simon Cockell, Paul Schreiber
+ * Author URI: http://knowledgeblog.org
+ *
+ * Copyright 2010. Phillip Lord (phillip.lord@newcastle.ac.uk)
+ * Simon Cockell (s.j.cockell@newcastle.ac.uk)
+ * Newcastle University.
+ * Paul Schreiber (paulschreiber@gmail.com)
+*/
+
+require_once __DIR__ . '/class-mathjax-latex.php';
