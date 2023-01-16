83f015a3a300919237fc20a87a4923df64268988
Paul Schreiber <paulschreiber@gmail.com>
2023-01-16 16:10:55 -0500

    config parameter cleanup
    * ensure there is a value for config parameter
    * use add_query_arg() to add config parameter

--- class-mathjax-latex.php
+++ class-mathjax-latex.php
@@ -210,7 +210,8 @@ class MathJax_Latex {
 			$mathjax_location = get_option( 'kblog_mathjax_custom_location' );
 		}
 
-		$mathjax_url = $mathjax_location . '?config=' . get_option( 'kblog_mathjax_config' );
+		$config      = get_option( 'kblog_mathjax_config' ) || 'default';
+		$mathjax_url = add_query_arg( 'config', $config, $mathjax_location );
 
 		wp_enqueue_script( 'mathjax', $mathjax_url, false, MATHJAX_PLUGIN_VERSION, false );
 
